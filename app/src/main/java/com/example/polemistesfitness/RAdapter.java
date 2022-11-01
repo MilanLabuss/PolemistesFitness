7db150e11aa43d2048507f497734b9474318d626
MilanLabus <MilanLabus28@gmail.com>
2022-11-01 13:34:19 +0000

    Added the Card
    Added the Recycleview cards for muscle groups

---
+++ app/src/main/java/com/example/polemistesfitness/RAdapter.java
@@ -0,0 +1,102 @@
+package com.example.polemistesfitness;
+
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RAdapter extends RecyclerView.Adapter<RAdapter.MyViewHolder> {
+
+
+    String names[];
+    Context context;
+    int images[];
+
+
+    public RAdapter(Context ct, String nm[], int img[] ){
+        names=nm;
+        images=img;
+        context=ct;
+    }
+
+
+    @NonNull
+    @Override
+    public RAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycle_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    //
+    @Override
+    public void onBindViewHolder(@NonNull RAdapter.MyViewHolder holder, int position) {
+//Setting the text to the textView from the names array
+        holder.Title.setText(names[holder.getAdapterPosition()]);
+        holder.myImg.setImageResource(images[holder.getAdapterPosition()]);
+        //onclick listener for the rows
+
+        holder.mylayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //might have to use getContext Here
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ChestExercisesActivity.class);
+                intent.putExtra("Name", names[holder.getAdapterPosition()]);
+                //intent.putExtra("myImg", images[holder.getAdapterPosition()]);
+                context.startActivity(intent);
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.length;
+    }
+
+    //here i will recieve the view and find the id of the UI elements
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView Title;
+        ImageView myImg;
+        //the main contraint layout of the row.xml which will implement on click listener
+        ConstraintLayout mylayout;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //find the ids of the UI elements
+            Title = itemView.findViewById(R.id.rowTitleText);
+            myImg = itemView.findViewById(R.id.rowImageView);
+            mylayout = itemView.findViewById(R.id.mainlayout);
+
+
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/polemistesfitness/WorkoutFragment.java
+++ app/src/main/java/com/example/polemistesfitness/WorkoutFragment.java
@@ -3,6 +3,8 @@ package com.example.polemistesfitness;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +13,33 @@ import android.view.ViewGroup;
 
 public class WorkoutFragment extends Fragment {
 
+    RecyclerView recycleview;
+
+    String musclegroupNames[];
+    int musclegroupImages[]={R.drawable.chest,R.drawable.arms,R.drawable.back, R.drawable.legs, R.drawable.shoulders};
+
+
+
     public WorkoutFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout, container, false);
+        View v =  inflater.inflate(R.layout.fragment_workout, container, false);
+        recycleview = (RecyclerView) v.findViewById(R.id.muscleGroupRecycle);
+
+
+        //Storing the String arrays in musclegroupname a
+        musclegroupNames = getResources().getStringArray(R.array.muscleGroup_names);
+
+        //If i get a bug it could be this getContext
+        RAdapter myadapter = new RAdapter(getContext(),musclegroupNames,musclegroupImages);
+        recycleview.setAdapter(myadapter);
+        //If i get a bug it could be this getContext
+        recycleview.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+        return v;
     }
 }
